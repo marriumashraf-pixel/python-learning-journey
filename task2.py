7fedec0bb2f17b014158c7cdf4ac6aa85a6f9c43
marrium ashraf <marriumashraf@gmail.com>
2026-08-14 06:54:19 -0700

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,7 @@
+sub1=int(input("Enter marks for DSA: "))
+sub2=int(input("Enter marks for Math: "))
+sub3=int(input("Enter marks for Physics: "))
+total=sub1+sub2+sub3
+print("Total marks:", total)
+percentage=(total/300)*100
+print("Percentage:", percentage, "%")
