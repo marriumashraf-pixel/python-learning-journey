515d6f82ab72b47174c3030091795add6833b65f
marrium ashraf <marriumashraf@gmail.com>
2026-08-20 02:11:58 -0700

    Add files via upload

---
+++ task7.py
@@ -0,0 +1,12 @@
+student={
+    "name": "mimi",
+    "age": "21",
+    "degree":"BSAI",
+    "University":"UET",
+    "CGPA":"2.7"
+    }
+print(student["name"])
+print(student["age"])
+print(student["degree"])
+print(student["University"])
+print(student["CGPA"])
